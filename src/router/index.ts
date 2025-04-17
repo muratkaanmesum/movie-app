1c52530a23a9ddf81bf6795319efe470b9101dcf
muratkaanmesum <kaanmesum@gmail.com>
2025-04-17 15:59:14 +0300

    start to movie app

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,12 +10,19 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/tv-shows',
+      name: 'TV Shows',
+      component: HomeView,
+    },
+    {
+      path: '/people',
+      name: 'People',
+      component: HomeView,
+    },
+    {
+      path: '/more',
+      name: 'More',
+      component: HomeView,
     },
   ],
 })
